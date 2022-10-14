bbed69f6aa943a1f2a34245d784e609796741079
leeconroy <leeconroy1977@yahoo.co.uk>
2022-10-14 14:03:59 +0100

    state added for sign up form

--- src/routes/Sign-In/SignIn.js
+++ src/routes/Sign-In/SignIn.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { useEffect} from 'react';
-import { getRedirectResult} from 'firebase/auth';
+import SignUp from '../../components/sign-up-form/SignUp';
 
 import { 
-  auth,
+
   signInWithGooglePopUp, 
   createUserDocumentFromAuth, 
-  signInWithGoogleRedirect,
+  
  } from '../../utils/firebase/Firebase';
 
 const SignIn = () => {
 
-  useEffect( () => {
-    async function fetchData(){
-    const response = await getRedirectResult(auth)
-    console.log(response)
-    console.log(fetchData())
-    
-    if(response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user)
-    }}
-
-  }, [])
+ 
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopUp();
@@ -34,7 +24,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+      <SignUp />
     </div>
   )
 }
